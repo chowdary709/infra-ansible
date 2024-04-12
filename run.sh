aab8b9cdcea69b03b788bfcc7b31fa8b1ed14d6d
chowdary709 <chandrahari321@gmail.com>
2024-04-12 13:56:33 +0530

    automated commit #241

--- run.sh
+++ run.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
+# ANSI color codes
+GREEN='\033[0;32m'
+BLUE='\033[0;34m'
+RED='\033[0;31m'
+NC='\033[0m' # No Color
+
 
 # Define function to run playbook for a specific role
 run_playbook() {
     role=$1
     echo "Running Ansible for '$role'..."
-    git pull ; ansible-playbook -i $role.roboshop.internal, -e "role_name=$role" main.yml
+    ${GREEN} git pull {NC}"; ansible-playbook -i $role.roboshop.internal, -e "role_name=$role" main.yml
 }
 
 # Main script
